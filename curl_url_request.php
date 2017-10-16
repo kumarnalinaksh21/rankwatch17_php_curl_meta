987d46085ebcf04b4281be5e61edf6077d53d788
Kumar Nalinaksh <32825207+kumarnalinaksh21@users.noreply.github.com>
2017-10-16 06:44:51 +0530

    Add files via upload

---
+++ curl_url_request.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Problem Statement 1 | rankwatch17_php_curl_meta</title>
+    <meta name="description" content="Marks: 5
+                                      Through CURL Request find meta keywords, meta description, title tag, IP address, Load
+                                      Time, HTTP Status, Internal & External Links of any URL.">
+    <meta name="keywords" content="Second round, RankWatch">
+    <link href='https://fonts.googleapis.com/css?family=Lobster|Open+Sans:400,400italic,300italic,300|Raleway:300,400,600' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+     </head>
+  <body>
+    <div class="content">
+      <div class="container wow fadeInUp delay-03s">
+        <div class="row">
+          <div class="logo text-center">
+            <img src="img/logo.png" alt="logo" width="150">
+            <h2>Problem Statement 1 | rankwatch17_php_curl_meta</h2>
+            <p>Marks: 5<br></br>
+               Through CURL Request find meta keywords, meta description, title tag, IP address, Load <br></br>
+               Time, HTTP Status, Internal and External Links of any URL.<br></br> </p>
+          </div>
+          <div> <h1  align="center">
+                Time Left Until Deadline !!!!!
+                </h1>
+          </div>
+          <div id="countdown" data-wow-delay=".3s" data-date="Oct 16, 2017 14:00:00">
+          </div>
+          <h2 class="subs-title text-center">Enter the website's URL</h2>
+         
+          <div class="subcription-info text-center">
+            <form class="subscribe_form" action="curl_url_request.php" method="post">
+              <input placeholder="Enter your website's URL..." name = "website" type="text">
+              <input type="submit">
+            </form>
+            <p class="sub-p">Ensure that the website has keyword and description meta's.</p>
+          </div>
+        </div>
+      </div>
+      
+      
+      
+    <div>
+    <?php 
+$webaddress = $_POST["website"];
+echo "<h3> Page details of the URL : $webaddress</h3>";
+echo "<br/><br/>";
+?>
+<?php include("function_title_desc_keyword.php"); ?>
+<?php include("function_ipaddress.php"); ?>
+<?php include("function_httpcode.php"); ?>
+<?php include("function_url_load_time.php"); ?>
+<?php include("function_url_list.php"); ?> 
+    </div>
+    <footer class="footer">
+      <div class="container">
+         <div class="row bort">
+
+           <h1>
+           Hope the project was to your liking :)
+           </h1>
+         </div>
+      </div>
+    </footer>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/wow.js"></script>
+    <script src="js/custom.js"></script>
+  </body>
+</html>-
